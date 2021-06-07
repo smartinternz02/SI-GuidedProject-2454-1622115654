4849d236c2db2c68dfa3fdb91646b0c6c93952d8
abhinay1442 <63285970+abhinay1442@users.noreply.github.com>
2021-06-07 14:18:15 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+Hello World app for running Python apps on Bluemix
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname("Python Flask App WFP"))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Python Flask App WFP',
+    version='1.0.0',
+    description='This app is to summarize an Article',
+    license='Apache-2.0'
+)
